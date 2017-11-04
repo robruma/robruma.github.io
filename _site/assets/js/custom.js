abc8412666fc60c72a5bf85b950327448d7aa412
Rob Ruma <robruma@users.noreply.github.com>
2017-11-03 23:04:33 -0400

    Fix 404 text and search

--- _site/assets/js/custom.js
+++ _site/assets/js/custom.js
@@ -38,5 +38,5 @@ function searchHandler() {
     var searchInput = document.getElementById('search-text');
     var text = searchInput.value;
     // add site:example.com in the placeholder
-    window.location.href = "https://www.google.com/search?q=site:invisiblerobots.org " + text;
+    window.location.href = "https://cse.google.com/cse/publicurl?cx=007797272485922345229:nrls_zubomg&q=" + text;
 }
